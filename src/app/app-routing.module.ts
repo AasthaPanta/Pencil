0ab11a89945feb99158c67c3142f011fcb7b368c
Aastha Panta <aasthapanta@Aasthas-MacBook-Pro.local>
2021-01-14 12:50:01 +0545

    styles added in login template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { EditorComponent } from './components/editor/editor.component';
+import { BaseComponent } from './components/base/base.component';
 
 const routes: Routes = [
-
+ 
   {
     path: 'editor',
     component: EditorComponent,
